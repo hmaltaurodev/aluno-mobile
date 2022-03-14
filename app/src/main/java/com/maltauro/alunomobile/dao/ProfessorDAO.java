290f9b429748773c1c069aa5ec610e9f9c132212
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-03-13 23:15:22 -0300

    feat(DAO/Professor): Implementado classe de acesso ao banco de dados.

--- app/src/main/java/com/maltauro/alunomobile/dao/ProfessorDAO.java
+++ app/src/main/java/com/maltauro/alunomobile/dao/ProfessorDAO.java
@@ -1,4 +1,52 @@
 package com.maltauro.alunomobile.dao;
 
+import android.util.Log;
+import com.maltauro.alunomobile.models.Professor;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfessorDAO {
+
+    public static long salvar(Professor professor) {
+        try {
+            return professor.save();
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao salvar o professor: " + ex.getMessage());
+            return -1;
+        }
+    }
+
+    public static Professor getProfessor(int id) {
+        try {
+            return Professor.findById(Professor.class, id);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao buscar o professor: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    public static List<Professor> getListAlunos(String where, String[] whererArgs, String orderBy) {
+        List<Professor> professores = new ArrayList<>();
+
+        try {
+            professores = Professor.find(Professor.class, where, whererArgs, null, orderBy, null);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao buscar a lista de professores: " + ex.getMessage());
+        }
+
+        return professores;
+    }
+
+    public static boolean delete(Professor professor) {
+        try {
+            return Professor.delete(professor);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao deletar o professor: " + ex.getMessage());
+            return false;
+        }
+    }
 }
