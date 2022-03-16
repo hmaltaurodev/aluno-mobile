2c47473096574221b4062fd48897edfb339b6c98
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-03-15 22:25:44 -0300

    feat(DAO/Disciplina): Implementado classe de acesso ao banco de dados.

--- app/src/main/java/com/maltauro/alunomobile/dao/DisciplinaDAO.java
+++ app/src/main/java/com/maltauro/alunomobile/dao/DisciplinaDAO.java
@@ -1,4 +1,52 @@
 package com.maltauro.alunomobile.dao;
 
+import android.util.Log;
+import com.maltauro.alunomobile.models.Disciplina;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisciplinaDAO {
+
+    public static long salvar(Disciplina disciplina) {
+        try {
+            return disciplina.save();
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao salvar o disciplina: " + ex.getMessage());
+            return -1;
+        }
+    }
+
+    public static Disciplina getDisciplina(int id) {
+        try {
+            return Disciplina.findById(Disciplina.class, id);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao buscar o disciplina: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    public static List<Disciplina> getListDisciplinas(String where, String[] whererArgs, String orderBy) {
+        List<Disciplina> disciplinas = new ArrayList<>();
+
+        try {
+            disciplinas = Disciplina.find(Disciplina.class, where, whererArgs, null, orderBy, null);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao buscar a lista de disciplinas: " + ex.getMessage());
+        }
+
+        return disciplinas;
+    }
+
+    public static boolean delete(Disciplina disciplina) {
+        try {
+            return Disciplina.delete(disciplina);
+        }
+        catch (Exception ex) {
+            Log.e("Erro", "Erro ao deletar o disciplina: " + ex.getMessage());
+            return false;
+        }
+    }
 }
