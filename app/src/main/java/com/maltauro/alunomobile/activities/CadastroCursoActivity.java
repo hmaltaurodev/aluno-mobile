9d0bebe0e817981d9673e6434ebfe9797d259e83
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-03-19 18:22:26 -0300

    feat(Acivity): Implementado activity de cadastro de curso.

---
+++ app/src/main/java/com/maltauro/alunomobile/activities/CadastroCursoActivity.java
@@ -0,0 +1,101 @@
+package com.maltauro.alunomobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.maltauro.alunomobile.R;
+import com.maltauro.alunomobile.dao.CursoDAO;
+import com.maltauro.alunomobile.enums.GrauAcademico;
+import com.maltauro.alunomobile.models.Curso;
+import com.maltauro.alunomobile.utils.Util;
+
+import java.util.Objects;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
+
+public class CadastroCursoActivity extends AppCompatActivity {
+
+    private TextInputEditText edtCodigoMECCurso;
+    private TextInputEditText edtDescricaoCurso;
+    private MaterialSpinner spGrauAcademico;
+    private LinearLayout lnCadastroCurso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_curso);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        edtCodigoMECCurso = findViewById(R.id.edt_codigo_mec_curso);
+        edtDescricaoCurso = findViewById(R.id.edt_descricao_curso);
+        lnCadastroCurso = findViewById(R.id.ln_cadastro_curso);
+
+        FloatingActionButton fab_grava_curso = findViewById(R.id.fab_grava_curso);
+        fab_grava_curso.setOnClickListener(view -> gravaCurso());
+
+        iniciaSpinner();
+    }
+
+    private void iniciaSpinner() {
+        spGrauAcademico = findViewById(R.id.sp_grau_academico);
+        ArrayAdapter adapterGrauAcademico = new ArrayAdapter(this, android.R.layout.simple_list_item_1, GrauAcademico.values());
+        spGrauAcademico.setAdapter(adapterGrauAcademico);
+    }
+
+    private boolean validaCampos() {
+        if (edtCodigoMECCurso.getText().toString().equals("")) {
+            edtCodigoMECCurso.setError("Informe o código MEC do curso!");
+            edtCodigoMECCurso.requestFocus();
+            return false;
+        }
+
+        if (edtDescricaoCurso.getText().toString().equals("")) {
+            edtDescricaoCurso.setError("Informe a descrição do curso!");
+            edtDescricaoCurso.requestFocus();
+            return false;
+        }
+
+        if (spGrauAcademico.getSelectedItemPosition() == 0) {
+            spGrauAcademico.setError("Selecione um grau acadêmico!");
+            spGrauAcademico.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void gravaCurso() {
+        if (validaCampos()) {
+            Curso curso = new Curso();
+
+            curso.setCodigoMEC(Integer.parseInt(edtCodigoMECCurso.getText().toString()));
+            curso.setDescricao(edtDescricaoCurso.getText().toString());
+            curso.setGrauAcademico((GrauAcademico) spGrauAcademico.getSelectedItem());
+
+            if (CursoDAO.salvar(curso) > 0) {
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+            else
+                Util.showSnackBar(lnCadastroCurso, "Erro ao salvar o curso, verifique o log!");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
